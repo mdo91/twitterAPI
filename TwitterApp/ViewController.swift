c2eb3e5113557f534756eb619e99fc688f03eca8
ASP <asp@asps-macbook-pro.local>
2018-12-15 17:01:58 +0300

    Initial Commit

---
+++ TwitterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterApp
+//
+//  Created by ASP on 12/15/18.
+//  Copyright © 2018 ASP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
